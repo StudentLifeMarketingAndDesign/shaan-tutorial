41adfe2a799c07ff2a848e3bdcf75712190d93c1
shaanamin13 <shaan-amin@uiowa.edu>
2014-10-17 12:52:07 -0500

    starting work on cfo conversion

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -25,13 +25,73 @@ class Page_Controller extends ContentController {
 	 *
 	 * @var array
 	 */
-	private static $allowed_actions = array (
-	);
+private static $allowed_actions = array('BrowserPollform');
 
 	public function init() {
 		parent::init();
 		// You can include any CSS or JS required by your project here.
 		// See: http://doc.silverstripe.org/framework/en/reference/requirements
+	}
+
+public function doBrowserPoll($data, $form){
+		$submission = new BrowserPollSubmission();
+		$form -> saveInto($submission);
+		$submission->write();
+		Session::set("BrowserPollVoted", true);
+		return $this->redirectBack();
+	}
+
+
+
+
+	public function LatestNews($num=5){
+		$holder = ArticleHolder::get()->First();
+		return ($holder) ? ArticlePage::get()->filter('ParentID',$holder->ID)->sort('Date DESC')->limit($num) : false;
+	}
+
+	
+
+	public function BrowserPollform(){
+		if(Session::get('BrowserPollVoted')) return false;
+
+		$fields = new FieldList(
+			new TextField('Name'),
+			new OptionsetField('Browser', 'Your Favourite Browser', array(
+				'Firefox' => 'Firefox',
+				'Chrome' => 'Chrome',
+				'Internet Explorer' => 'Internet Explorer',
+				'Safari'=>'Safari',
+				'Opera'=>'Opera',
+				'Lynx'=>'Lynx'
+
+				))
+
+
+			);
+
+		$actions = new FieldList(
+				new FormAction('doBrowserPoll', 'Submit')
+			);
+		$validator = new RequiredFields('Name', 'Browser');
+		
+		return new Form($this, 'BrowserPollform', $fields,$actions,$validator);
+			
+
+
+	}
+
+	public function BrowserPollResults(){
+		$submissions = new GroupedList(BrowserPollSubmission::get());
+		$total = $submissions->Count();
+		$list = new ArrayList();
+		foreach($submissions->groupBy('Browser') as $browserName => $browserSubmissions){
+			$percentage = (int) ($browserSubmissions->Count()/$total * 100);
+			$list->push(new ArrayData(array(
+					'Browser' => $browserName,
+					'Percentage' => $percentage
+				)));
+		}
+		return $list;
 	}
 
 }
